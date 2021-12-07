6964ce0db48937f1497240b7156caa8f8ffa627a
SansPapyrus683 <kevinsheng2005@gmail.com>
2021-12-06 21:20:44 -0800

    day 7

---
+++ 2021/day7/here_to_stay.py
@@ -0,0 +1,25 @@
+from statistics import median
+
+with open("is_the_new_bird.txt") as read:
+    crabs = [int(i) for i in read.readline().split(",")]
+
+"""
+this approach is from this thread here
+https://math.stackexchange.com/questions/113270
+idek how it 100% works, but it does, so i'm using it
+"""
+med_pos = median(crabs)
+p1_fuel = int(sum(abs(c - med_pos) for c in crabs))  # should always be an integer
+print(f"thank god for this obscure math black magic thing: {p1_fuel}")
+
+# lmao idk if there's a better way to do this
+min_pos = min(crabs)
+max_pos = max(crabs)
+p2_fuel = float('inf')
+for p in range(min_pos, max_pos + 1):
+    total = 0
+    for c in crabs:
+        dist = abs(c - p)
+        total += dist * (dist + 1) // 2
+    p2_fuel = min(p2_fuel, total)
+print(f"yoooo 1st time i got top 100 for both stars: {p2_fuel}")
