6a9e4eb2698bb1eb506da30441286cb2f3ed1bcd
SansPapyrus683 <kevinsheng2005@gmail.com>
2021-12-06 16:05:39 -0800

    turns out it's vents not wires lol

--- 2021/day5/walking_in_a.py
+++ 2021/day5/walking_in_a.py
@@ -24,40 +24,40 @@ def two_cross_num(cross_num: [[int]]) -> int:
 
 
 with open("winter_wonderland.txt") as read:
-    wires = []
+    vents = []
     for l in read.readlines():
         l = l.strip().split(' -> ')
-        wires.append([
+        vents.append([
             Point(*[int(i) for i in l[0].split(',')]),
             Point(*[int(i) for i in l[1].split(',')])
         ])
 
 # not sure if there's negatives, not gonna take any changes
-min_x = min(min(w[0].x, w[1].x) for w in wires)
-min_y = min(min(w[0].y, w[1].y) for w in wires)
-for w in wires:
-    w[0].x -= min_x
-    w[1].x -= min_x
-    w[0].y -= min_y
-    w[1].y -= min_y
-max_x = max(max(w[0].x, w[1].x) for w in wires)
-max_y = max(max(w[0].y, w[1].y) for w in wires)
+min_x = min(min(v[0].x, v[1].x) for v in vents)
+min_y = min(min(v[0].y, v[1].y) for v in vents)
+for v in vents:
+    v[0].x -= min_x
+    v[1].x -= min_x
+    v[0].y -= min_y
+    v[1].y -= min_y
+max_x = max(max(w[0].x, w[1].x) for w in vents)
+max_y = max(max(w[0].y, w[1].y) for w in vents)
 
 straight_crosses = [[0 for _ in range(max_y + 1)] for _ in range(max_x + 1)]
-for w in wires:
-    w.sort()
-    if w[0].x == w[1].x:
-        for y in range(w[0].y, w[1].y + 1):
-            straight_crosses[w[0].x][y] += 1
-    elif w[0].y == w[1].y:
-        for x in range(w[0].x, w[1].x + 1):
-            straight_crosses[x][w[0].y] += 1
+for v in vents:
+    v.sort()
+    if v[0].x == v[1].x:
+        for y in range(v[0].y, v[1].y + 1):
+            straight_crosses[v[0].x][y] += 1
+    elif v[0].y == v[1].y:
+        for x in range(v[0].x, v[1].x + 1):
+            straight_crosses[x][v[0].y] += 1
 print(f"god input for this problem was hell: {two_cross_num(straight_crosses)}")
 
 all_crosses = [[0 for _ in range(max_y + 1)] for _ in range(max_x + 1)]
-for w in wires:
-    x_add = w[1].x - w[0].x
-    y_add = w[1].y - w[0].y
+for v in vents:
+    x_add = v[1].x - v[0].x
+    y_add = v[1].y - v[0].y
     if x_add < 0:
         x_add = -1
     elif x_add > 0:
@@ -68,8 +68,8 @@ for w in wires:
     elif y_add > 0:
         y_add = 1
 
-    curr = deepcopy(w[0])
-    while curr != w[1]:
+    curr = deepcopy(v[0])
+    while curr != v[1]:
         all_crosses[curr.x][curr.y] += 1
         curr.x += x_add
         curr.y += y_add
