4d9c8992e099940a287117f9e06e54e5052c5feb
SansPapyrus683 <kevinsheng2005@gmail.com>
2021-12-22 13:03:48 -0800

    stuff

--- 2021/day22/p1_well_pretend.py
+++ 2021/day22/p1_well_pretend.py
@@ -6,11 +6,11 @@ operations = []
 with open("parson_brown.txt") as read:
     for l in read.readlines():
         a, b = l.strip().split()
-        on = a == 'on'
-        b = b.split(',')
-        b = [x.split('=') for x in b]
+        on = a == "on"
+        b = b.split(",")
+        b = [x.split("=") for x in b]
         for i in range(len(b)):
-            b[i][1] = [int(y) for y in b[i][1].split('..')]
+            b[i][1] = [int(y) for y in b[i][1].split("..")]
         operations.append([on, np.array([x[1] for x in b])])
 
 min_val = -RANGE
@@ -22,6 +22,6 @@ for _, o in operations:
 relevant = np.zeros((RANGE * 2, RANGE * 2, RANGE * 2)).astype(bool)
 for val, o in operations:
     x, y, z = o
-    # don't worry if it goes out of range, python handles it just fine
+    # don"t worry if it goes out of range, python handles it just fine (idk how)
     relevant[x[0]:x[1] + 1, y[0]:y[1] + 1, z[0]:z[1] + 1] = val
 print(f"lmao another \"oh p1 isn't so bad\" problem: {relevant.sum()}")
