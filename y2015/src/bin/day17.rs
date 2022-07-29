8689a90e3f2f10b808fa9455dd9001697c0c2bc8
SansPapyrus683 <55369003+SansPapyrus683@users.noreply.github.com>
2022-07-29 12:09:06 -0700

    Create day17.rs

---
+++ y2015/src/bin/day17.rs
@@ -0,0 +1,33 @@
+use std::fs;
+use itertools::Itertools;
+
+const EGGNOG: u32 = 150;
+
+fn main() {
+    let read = fs::read_to_string("input/day17.txt").expect("you done messed up");
+    let mut containers = Vec::new();
+    for c in read.lines() {
+        containers.push(c.parse::<u32>().unwrap());
+    }
+
+    let mut comb_num = 0;
+    let mut min_comb_num = 0;
+    let mut found_min = false;
+    for sz in 1..=containers.len() {
+        let mut this_comb_num = 0;
+        for sub in containers.iter().combinations(sz) {
+            if sub.iter().copied().sum::<u32>() == EGGNOG {
+                comb_num += 1;
+                this_comb_num += 1;
+            }
+        }
+        
+        if this_comb_num != 0 && !found_min {
+            found_min = true;
+            min_comb_num = this_comb_num;
+        }
+    }
+
+    println!("{comb_num}");
+    println!("{min_comb_num}");
+}
