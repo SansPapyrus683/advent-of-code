48212d51e78bb1c9fc595af56a818e2dd168b3cc
SansPapyrus683 <55369003+SansPapyrus683@users.noreply.github.com>
2022-08-06 10:31:51 -0700

    day 25 with ndarray

--- y2015/src/bin/day25.rs
+++ y2015/src/bin/day25.rs
@@ -1,4 +1,6 @@
-const POS: (u32, u32) = (3010, 3019);
+use ndarray::prelude::*;
+
+const POS: (usize, usize) = (3010, 3019);
 const START: i32 = 20151125;
 
 fn next_val(curr: i32) -> i32 {
@@ -6,7 +8,7 @@ fn next_val(curr: i32) -> i32 {
     (curr as i64 * 252533 % 33554393) as i32
 }
 
-fn next_pos(pos: &(u32, u32)) -> (u32, u32) {
+fn next_pos(pos: &(usize, usize)) -> (usize, usize) {
     if pos.0 == 1 {
         (pos.1 + 1, 1)
     } else {
@@ -15,20 +17,16 @@ fn next_pos(pos: &(u32, u32)) -> (u32, u32) {
 }
 
 fn main() {
-    let mut at = (1, 1);
     // this should be big enough
-    let mut paper = vec![vec![-1; POS.1 as usize * 3]; POS.0 as usize * 3];
+    let mut paper = Array::<i32, _>::zeros((POS.0 * 3, POS.1 * 3));
 
-    paper[at.0 as usize][at.1 as usize] = START;
-    let mut prev = at;
-    at = next_pos(&at);
-    while paper[POS.0 as usize][POS.1 as usize] == -1 {
-        paper[at.0 as usize][at.1 as usize] = next_val(
-            paper[prev.0 as usize][prev.1 as usize]
-        );
-        prev = at;
-        at = next_pos(&at);
+    let mut at = (1, 1);
+    paper[[at.0, at.1]] = START;
+    while at != POS {
+        let next = next_pos(&at);
+        paper[[next.0, next.1]] = next_val(paper[[at.0, at.1]]);
+        at = next;
     }
 
-    println!("machine code: {}", paper[POS.0 as usize][POS.1 as usize])
+    println!("machine code: {}", paper[[POS.0, POS.1]])
 }
