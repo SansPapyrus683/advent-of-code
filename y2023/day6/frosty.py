7cffcd1991ae218eba4147495a935a39868c67e7
Kevin Sheng <kevinsheng2005@gmail.com>
2023-12-05 21:15:19 -0800

    day 6...

--- y2023/day6/frosty.py
+++ y2023/day6/frosty.py
@@ -1,69 +1,23 @@
-from collections import defaultdict
-from itertools import permutations, combinations
-from copy import deepcopy
+from math import sqrt, ceil, floor
 
 
-# region utility
-def group(seq, sep):
-    ret = []
-    for el in seq:
-        if el == sep:
-            yield ret
-            ret = []
-        else:
-            ret.append(el)
-    yield ret
-
-
-def chunks(lst: list, n: int):
-    """source: https://stackoverflow.com/questions/312443"""
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
-
-
-def neighbors4raw(r: int, c: int) -> list[tuple[int, int]]:
-    return [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]
-
-
-def neighbors4(r: int, c: int, r_max: int, c_max: int) -> list[tuple[int, int]]:
-    return [p for p in [
-        (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1),
-    ] if 0 <= p[0] < r_max and 0 <= p[1] < c_max]
-
-
-def neighbors8raw(r: int, c: int) -> list[tuple[int, int]]:
-    return [
-        (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1),
-        (r + 1, c + 1), (r - 1, c - 1), (r - 1, c + 1), (r + 1, c - 1),
-    ]
-
-
-def neighbors8(r: int, c: int, r_max: int, c_max: int) -> list[tuple[int, int]]:
-    return [p for p in [
-        (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1),
-        (r + 1, c + 1), (r - 1, c - 1), (r - 1, c + 1), (r + 1, c - 1),
-    ] if 0 <= p[0] < r_max and 0 <= p[1] < c_max]
-
-
-def dist(p1: tuple[int, int], p2: tuple[int, int]) -> int:
-    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-
-
-def inter(a_start: int, a_end: int, b_start: int, b_end: int) -> tuple[int, int] | None:
-    """intersection of [a_start, a_end] and [b_start, b_end]"""
-    start = max(a_start, b_start)
-    end = min(a_end, b_end)
-    return None if start > end else (start, end)
-
-
-def sign(n: int) -> int:
-    if n == 0:
-        return 0
-    return -1 if n < 0 else 1
-# endregion
+def valid_times(time: int, dist: int) -> int:
+    x = (-time + sqrt(time ** 2 - 4 * dist)) / -2
+    y = (-time - sqrt(time ** 2 - 4 * dist)) / -2
+    return floor(y) - ceil(x) + 1
 
 
 with open("the_snowman.txt") as read:
-    for g in group(read.readlines(), "\n"):
-        for i in g:
-            pass
+    time = [int(x) for x in read.readline().split(":")[1].split()]
+    distance = [int(x) for x in read.readline().split(":")[1].split()]
+
+p1_prod = 1
+for t, d in zip(time, distance):
+    p1_prod *= valid_times(t, d)
+
+big_time = int("".join(map(str, time)))
+big_dist = int("".join(map(str, distance)))
+p2_amt = valid_times(big_time, big_dist)
+
+print(f"BRO WHAT THE HELL: {p1_prod}")
+print(f"I THOUGHT P2 INVOLVED MATH WHEN BRUTE FORCE WORKED: {p2_amt}")
