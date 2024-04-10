1370f38955b89761313d746bb800dd8210f8270c
Kevin Sheng <kevinsheng2005@gmail.com>
2024-04-09 19:28:49 -0700

    start up 2018

---
+++ y2018/rust/src/bin/day1.rs
@@ -0,0 +1,27 @@
+use std::collections::HashSet;
+use std::fs;
+
+fn main() {
+    let chances: Vec<i32> = fs::read_to_string("../input/day1.txt")
+        .unwrap()
+        .split_whitespace()
+        .map(|i| i.parse::<i32>().unwrap())
+        .collect();
+
+    let mut curr_freq = 0;
+    let mut seen_freqs = HashSet::from([curr_freq]);
+    let mut seen_twice = i32::MIN;
+    while seen_twice == i32::MIN {
+        for f in &chances {
+            curr_freq += f;
+            if seen_freqs.contains(&curr_freq) && seen_twice == i32::MIN {
+                seen_twice = curr_freq;
+                break;
+            }
+            seen_freqs.insert(curr_freq);
+        }
+    }
+
+    println!("final frequency: {}", chances.iter().sum::<i32>());
+    println!("first frequency seen twice: {seen_twice}");
+}
